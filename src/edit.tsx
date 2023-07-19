f1a5b91ed41b7244cfb97487f5cc1fdf497896a3
Tetsuaki Hamano <tetsuaki.hamano@gmail.com>
2023-07-19 20:30:29 +0900

    Fix lint error

--- src/edit.tsx
+++ src/edit.tsx
@@ -9,7 +9,6 @@ import type { BlockEditProps } from '@wordpress/blocks';
 /**
  * Internal dependencies
  */
-import './editor.scss';
 import Piano from './components/piano';
 import { DEFAULT_SETTINGS } from './constants';
 import type { BlockAttributes } from './constants';
