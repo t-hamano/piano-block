6ca92419badb49f60e53434382edd7ea3ce50c3c
Aki Hamano <54422211+t-hamano@users.noreply.github.com>
2024-10-11 14:02:54 +0900

    Improve performance (#57)

--- src/components/piano/index.tsx
+++ src/components/piano/index.tsx
@@ -12,7 +12,7 @@ import { useEffect, useState, useRef } from '@wordpress/element';
 /**
  * Internal dependencies
  */
-import { DEFAULT_ENVELOPE, INSTRUMENTS } from '../../constants';
+import { DEFAULT_ENVELOPE, INSTRUMENTS, LATENCY } from '../../constants';
 import { KEYBOARD_LAYOUTS } from '../../keyboard-layout';
 import Loading from '../loading';
 import Keyboard from '../keyboard';
@@ -137,7 +137,7 @@ const Piano = ( { settings, onChange }: Props ) => {
 			targetKey.octave + octaveOffset + instrumentOctaveOffset
 		}`;
 
-		piano.triggerAttack( targetNote );
+		piano.triggerAttack( targetNote, piano.context.currentTime + LATENCY );
 		setActiveKeys( [ ...activeKeys, targetKey ] );
 	};
 
@@ -177,7 +177,7 @@ const Piano = ( { settings, onChange }: Props ) => {
 		const targetNote = `${ note }${ octave + octaveOffset + instrumentOctaveOffset }`;
 
 		if ( useSustainPedal && instrument !== 'synthesizer' ) {
-			piano.triggerAttack( targetNote );
+			piano.triggerAttack( targetNote, piano.context.currentTime + LATENCY );
 		} else {
 			piano.triggerAttackRelease( targetNote, 0.2 );
 		}
