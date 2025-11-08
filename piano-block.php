568ed36cce0c2fa0a3b793a06aa108d3be309dfb
Aki Hamano <tetsuaki.hamano@gmail.com>
2025-11-08 19:15:00 +0900

    Drop support for PHP 7

--- piano-block.php
+++ piano-block.php
@@ -3,7 +3,7 @@
  * Plugin Name: Piano Block
  * Description: Can play a variety of tones using the piano keyboard.
  * Requires at least: 6.8
- * Requires PHP: 7.4
+ * Requires PHP: 8.0
  * Version: 2.7.0
  * Author: Aki Hamano
  * Author URI: https://github.com/t-hamano
